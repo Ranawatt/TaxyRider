8a73a7a63ecfd882148e56a9677de26021cb9227
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-05-25 23:45:44 +0530

    Added Support ActionBar to Memory Activity

--- app/src/main/java/com/example/taxyrider/memoryoverload/MemoryActivity.kt
+++ app/src/main/java/com/example/taxyrider/memoryoverload/MemoryActivity.kt
@@ -1,13 +1,19 @@
 package com.example.taxyrider.memoryoverload
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.taxyrider.R
 
+
 class MemoryActivity : AppCompatActivity() {
+
+    val NO_OF_TEXTVIEWS_ADDED = 10000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memory)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }

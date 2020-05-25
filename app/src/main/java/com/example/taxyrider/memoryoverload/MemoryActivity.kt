f99485c3248e4502f02465afea1f94bdaf52c049
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-05-25 23:39:10 +0530

    Created Memory Overload package

---
+++ app/src/main/java/com/example/taxyrider/memoryoverload/MemoryActivity.kt
@@ -0,0 +1,13 @@
+package com.example.taxyrider.memoryoverload
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.taxyrider.R
+
+class MemoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memory)
+    }
+}
